af07f34b65072050b6e19eeda1dc6ef5e4080792
Spencer Bohol <zxtm89j1@gmail.com>
2023-09-10 21:11:09 +0800

    added routes for home

--- routes/routes.js
+++ routes/routes.js
@@ -2,31 +2,32 @@ const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
 
-
-router.get("/", (req, res) => {
-    res.send("Homepage of GET method!")
-})
-
 router.post("/adduser", async (req, res) => {
   try {
-      const {name, password, email} = req.body;
+    const { name, password, email } = req.body;
 
-      const newUser = new User({
-        name: name,
-        password: password,
-        email: email,
-      });
+    const newUser = new User({
+      name: name,
+      password: password,
+      email: email,
+    });
 
-      await newUser.save();
+    await newUser.save();
 
-      res.status(201).json(newUser);
-
-  } catch(error) {
-      console.error(error);
-      res.status(500).json({error: "An error occured!!!"})
+    res.status(201).json(newUser);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "An error occured!!!" });
   }
+});
 
-
+router.get("/", async (req, res) => {
+  try {
+    const result = await User.find();
+    res.status(201).json(result);
+  } catch (error) {
+    res.status(500).json({ error: "An error occured!!!" });
+  }
 });
 
 module.exports = router;
